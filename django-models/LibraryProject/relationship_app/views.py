148670a8448411a9049f0294af05fee4f55d6764
Moses Kariuki <kariukifx471@gmail.com>
2026-02-20 06:07:09 +0300

    Fix Task 1: Adjusted imports to satisfy checker requirements

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,21 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library
 
 
-# ---------------------------------------------------
-# Function-Based View
-# Lists all books
-# ---------------------------------------------------
+# Function-based view
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
-# ---------------------------------------------------
-# Class-Based View
-# Displays details of a specific library
-# ---------------------------------------------------
+# Class-based view
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
