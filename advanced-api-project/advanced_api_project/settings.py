229af885c41cb4120bab81730c79b8b427145108
Moses Kariuki <kariukifx471@gmail.com>
2026-03-07 12:24:49 +0300

    Task 2: Implemented filtering, searching, and ordering for BookListView

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',  
+    'rest_framework',
+    'django_filters', 
     'api',  
 ]
 
@@ -125,5 +126,10 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
     ],
 }
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,14 +1,21 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+from .models import Book
 from .serializers import BookSerializer
 
-# ListView - Retrieves all books (accessible to everyone)
+# ListView - Retrieves all books with filtering, searching, and ordering
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['title', 'author', 'publication_year']  # Filter by these fields
+    search_fields = ['title', 'author__name']                   # Search by title or author name
+    ordering_fields = ['title', 'publication_year']             # Order by these fields
 
-# DetailView - Retrieves a single book by ID (accessible to everyone)
+# DetailView - Retrieves a single book by ID
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
